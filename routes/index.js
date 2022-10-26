291c9f4620a60b61fc8752ae0623f36069454105
Tim Krause <tim@iamparagon.com>
2022-10-25 23:10:39 -0400

    update node route index

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,13 @@ const bookRoutes = require('./books');
 router.use('/api/books', bookRoutes);
 
 // If no API routes are hit, send the React app
-router.use(function(req, res) {
-	res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 module.exports = router;
