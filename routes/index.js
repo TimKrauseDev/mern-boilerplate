8e8c47dd2e8b5ed4857a6bd2043adbca68ec29a7
Tim Krause <tim@iamparagon.com>
2022-10-25 23:13:55 -0400

    last try

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,5 @@ const bookRoutes = require('./books');
 
 router.use('/api/books', bookRoutes);
 
-// If no API routes are hit, send the React app
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 module.exports = router;
+
--- server.js
+++ server.js
@@ -15,7 +15,14 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Bootstrap server
 app.listen(PORT, () => {
